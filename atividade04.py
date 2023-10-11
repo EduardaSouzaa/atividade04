268bbd84b6cc3e6ce863de9e311fcff53211fff8
EduardaSouzaa <antonia.lima489@aluno.ce.gov.br>
2023-10-11 11:37:39 -0300

    atividade04

---
+++ atividade04.py
@@ -0,0 +1,6 @@
+# Escreva um numero e veja qual o seu antecessor e seu sucessor! 
+print("Descruba o sucessor e antecessor de um número qualquer") 
+numero = float(input("digite o numéro: "))
+sucessor = numero+1
+antecessor = numero-1
+print(f"sucessor do seu 8numero é {sucessor} e o antecessor do numero é {antecessor}")
